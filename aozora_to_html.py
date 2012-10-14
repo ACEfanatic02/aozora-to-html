f7be5275b8c98773f1b93e8b0df1b4de2aec3ffe
ACEfanatic02 <acefanatic02@gmail.com>
2012-10-14 03:30:19 -0400

    BeautifulSoup added, more project setup

--- aozora_to_html.py
+++ aozora_to_html.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Aozora To Html
+"""Aozora To HTML
 
 A suite of tools to turn 青空文庫-formatted text into an easily readable HTML
-document.
+document.  The goal is to create a format that is comfortable to read, even on
+a computer screen.
 
-Author: ACEfanatic02 (acefanatic02@gmail.com)
-Credits: Jeremy Davidson (BlackDragonHunt) -- for the original suite
-        of Tadoku Tools on which this project is based.
-Disclaimer: 
+TOOLS:
+-- 1. Rubificator
+    
+    Turns 青空文庫 furigana into actual ruby tags (supported natively in Chrome,
+    and via extensions in Firefox.)
+
+Author:     ACEfanatic02 (acefanatic02@gmail.com)
+Credits:  - Jeremy Davidson (BlackDragonHunt) -- for the original suite
+            of Tadoku Tools on which this project is based.
+          - Brian Nez (thedude@bri1.com) -- for 'pretty.py', used for color in 
+            the testing suite.
+          - Uses BeautifulSoup for HTML processing.
+Disclaimer: No warranty, provided 'as-is', etc.  Thou shalt not blame me if 
+            this script breaks or damages your machine or data.
 """
 
 import sys
+import os.path
+import codecs
+
+import lib.BeautifulSoup
+
+import aozora
